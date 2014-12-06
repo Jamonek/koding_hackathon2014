d19f28718bd9151ddfd2d1aa9f1c067615e86209
Jamone Kelly <xg.monie089@gmail.com>
2014-12-06 14:48:11 -0500

    Testing

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,7 +1,16 @@
 <div class="container-fluid">
 	<div class="row">
-		<button type="button" class="btn btn-success">Login</button>
-		<button type="button" class="btn btn-success">Sign Up</button>
-		<button type="button" class="btn btn-success">Guest User</button>
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Welcome etc etc etc
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<button type="button" class="btn btn-success btn-large">Login</button>
+			<button type="button" class="btn btn-success btn-large">Sign Up</button>
+			<button type="button" class="btn btn-success btn-large">Guest User</button>
+		</div>
 	</div>
 </div>
