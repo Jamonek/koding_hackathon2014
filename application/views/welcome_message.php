75c558492ca09ed707631a117f76b6c282037f33
Jamone Kelly <xg.monie089@gmail.com>
2014-12-06 14:38:03 -0500

    Initial Start of our project

---
+++ application/views/welcome_message.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+	<div class="row">
+		<button type="button" class="btn btn-success">Login</button>
+		<button type="button" class="btn btn-success">Sign Up</button>
+		<button type="button" class="btn btn-success">Guest User</button>
+	</div>
+</div>
