8b1304edc378173e51423376efb6d447d1be89a6
Jamone Kelly <xg.monie089@gmail.com>
2014-12-06 14:51:22 -0500

    Switch from full page view
    Switch to a contained page view and fix button sizes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container">
 	<div class="row">
 		<div class="panel panel-default">
 			<div class="panel-body">
@@ -8,9 +8,9 @@
 	</div>
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
-			<button type="button" class="btn btn-success btn-large">Login</button>
-			<button type="button" class="btn btn-success btn-large">Sign Up</button>
-			<button type="button" class="btn btn-success btn-large">Guest User</button>
+			<button type="button" class="btn btn-success btn-lg">Login</button>
+			<button type="button" class="btn btn-success btn-lg">Sign Up</button>
+			<button type="button" class="btn btn-success btn-lg">Guest User</button>
 		</div>
 	</div>
 </div>
