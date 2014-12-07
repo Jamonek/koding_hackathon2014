68dc81faefe7c66451ff0fcc76d9cea9900813c4
Jamone Kelly <xg.monie089@gmail.com>
2014-12-07 18:40:10 -0500

    preventive measures again

--- application/controllers/work.php
+++ application/controllers/work.php
@@ -5,6 +5,9 @@ class Work extends CI_Controller {
 public function index()
 	{
 		// only for logged in users
+		if(!$this->users->loggedIn())
+  			redirect('login');
+  		
 		$data['site_sub_page'] = 'Work';
 		$data['active_nav'] = 'my_work';
 		$this->load->view('includes/header', $data);
