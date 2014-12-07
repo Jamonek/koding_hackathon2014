85dcd233082f1754c17c6bc671065e6da1b1ea2e
Keheira <Keheira@users.noreply.github.com>
2014-12-07 08:19:33 -0600

    Create login.php

---
+++ application/views/login.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <!-- Contact Form -->
+  <form action="" method="GET" role="form"> <!--Dont know what acton to use -->
+    <div class="form-group">
+      <label for="username">Username</label>
+      <input type="text" class="form-control" id="username" name="name" placeholder="Goat1">
+    </div>
+    <div class="form-group">
+      <label for="accountPass">Password</label>
+      <input type="password" class="form-control" id="accountPass" name="password" placeholder="&bullet;&bullet;&bullet;&bullet;&bullet;&bullet;">
+    </div>
+    <div class="form-group">
+      <input type="submit" class="form-control btn btn-info btn-lg" name="submit" value="Send">
+    </div>
+  </form>
+</div>
