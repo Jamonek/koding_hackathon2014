400f3c3f2cf6161cf30ad27ab5d0abd4913c7d47
Keheira <Keheira@users.noreply.github.com>
2014-12-07 02:40:47 -0500

    Create contact.php

---
+++ application/views/contact.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<!-- Contact Form -->
+	<form action="" method="POST" role="form"> <!--Dont know what acton to use -->
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" class="form-control" id="email" name="email" placeholder="john@domain.com">
+		</div>
+		<div class="form-group">
+			<label for="msg">Message</label>
+			<input type="text" class="form-control" id="msg" name="msg">
+		</div>
+		<div class="form-group">
+			<input type="submit" class="form-control btn btn-info btn-lg" name="submit" value="Send">
+		</div>
+	</form>
+</div>
