927bdb975bd79ae552a5d68c3b0e9c45e4ccf675
Keheira <Keheira@users.noreply.github.com>
2014-12-07 02:01:27 -0600

    Create about.php

---
+++ application/controllers/about.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+  class About extends CI_Controller {
+    public function index(){
+      $this->load->view('includes/header');
+      $this->load->view('signup.php');
+      $this->load->view('includes/footer');
+    }
+  }
+}
