e7784775e781a3657495f488bc5aead3ab192822
Jamone Kelly <xg.monie089@gmail.com>
2014-12-06 20:52:46 -0500

    Add in signup shell

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,11 +1,24 @@
 <div class="container">
 	<div class="row">
 		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Welcome</h3>
+			</div>
 			<div class="panel-body">
 				Welcome etc etc etc
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">How to use</h3>
+			</div>
+			<div class="panel-body">
+				Howv to use blah blah blah
+			</div>
+		</div>
+	</div>	
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
 			<button type="button" class="btn btn-success btn-lg">Login</button>
