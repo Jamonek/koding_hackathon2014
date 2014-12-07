dab4ccdfd902fb1f468ed14247571aaa5f4f6563
Jamone Kelly <xg.monie089@gmail.com>
2014-12-07 16:25:00 -0500

    formar test

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,12 +1,15 @@
+    <div id="push"></div>
+    </div>
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <div class="footer">
-		<center>
-			<div style="color: white;">Team Goat | &copy 2014</div>
-		</center>
-	</div>
+	<div id="footer">
+      <div class="container">
+        <p class="muted"><div style="color: white;">Team Goat | &copy 2014</div></p>
+      </div>
+    </div>
   </body>
 </html>
 
