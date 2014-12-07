8d4c04247ba0ea134f1402e47119b464e3884563
Jamone Kelly <xg.monie089@gmail.com>
2014-12-07 16:18:32 -0500

    Format test

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -2,12 +2,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <footer>
+		<center>
+			<div style="color: white;">Team Goat | &copy 2014</div>
+		</center>
+	</footer>
   </body>
 </html>
-<br>
-<br>
-<footer>
-<center>
-	Team Goat | &copy 2014
-</center>
-</footer>
+
