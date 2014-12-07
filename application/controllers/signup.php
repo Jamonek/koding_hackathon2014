6a11adcc2206a5298e7cbf9ae44b57ef6976dffd
Jamone Kelly <xg.monie089@gmail.com>
2014-12-07 16:00:03 -0500

    Add missing parameter

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -6,7 +6,7 @@ class Signup extends CI_Controller {
 	{
 		$data['site_sub_page'] = 'Sign Up';
 		$data['active_nav'] = 'signup';
-		$this->load->view('includes/header');
+		$this->load->view('includes/header', $data);
 		$this->load->view('signup.php');
 		$this->load->view('includes/footer');
 		
