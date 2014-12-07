563ed30daeca948f6c42cbd6a8651786a2a4482a
dijdev <danieljonesdev@gmail.com>
2014-12-07 13:37:10 -0500

    Work work work work, home page is now called work

--- application/controllers/work.php
+++ application/controllers/work.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Work extends CI_Controller {
 
 public function index()
 	{
 		$this->load->view('includes/header');
-		$this->load->view('home_view');
+		$this->load->view('work');
 		$this->load->view('includes/footer');
 	}
 }
