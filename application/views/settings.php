7c3b1f059224d32a2ce8289f162ec45414d35934
Keheira <Keheira@users.noreply.github.com>
2014-12-07 19:32:00 -0600

    Create settings.php

---
+++ application/views/settings.php
@@ -0,0 +1,19 @@
+<!-- About Page -->
+<div class="well">
+  <div class="setting-group">
+    <label for="username"> Username</label>
+    <input type="txt" class="setting-control" id="username" name="username">
+  </div>
+  <div class="setting-group">
+    <label for="email">New Email</label>
+    <input type="email" class="setting-control" id="email" name="email">
+  </div>
+  <div class="btn-group" data-toggle="buttons-radio">
+    <label>Get Email notifications?</label>
+    <button type="button" class="btn btn-primary">Yes</button>
+    <button type="button" class="btn btn-primary">No</button>
+  </div>
+  <div class="setting-group">
+    <input type="submit" class="btn btn-info btn-lg btn-spacing" name="submit" value="Save">
+  </div>
+</div>
