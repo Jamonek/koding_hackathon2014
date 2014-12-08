0e6af9d70a8b349b6da4dd342c797be59dc71eab
Jamone Kelly <xg.monie089@gmail.com>
2014-12-07 20:25:49 -0500

    Testing

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -4,7 +4,8 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?=base_url();?>js/bootstrap.min.js"></script>
+    <script src="<?=base_url();?>js/holder.js"></script>
 	<div class="footer">
 		<div id="push"></div>
         <p class="muted"><div style="color: white;"><center>Team Goat | &copy 2014</center></div></p>
