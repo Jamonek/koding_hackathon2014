6739f8bb24a8366e508327f4ce0e7d649e285c28
Keheira <Keheira@users.noreply.github.com>
2014-12-07 19:44:19 -0600

    Create work2.php

---
+++ application/controllers/work2.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Work2 extends CI_Controller {
+  public function index(){
+  // guest users
+  redirect('login');
+  $data['site_sub_page'] = 'Work2';
+  $data['active_nav'] = 'my_work';
+  $this->load->view('includes/header', $data);
+  $this->load->view('work2');
+  $this->load->view('includes/footer');
+  }
+}
